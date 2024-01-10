cd16906906f997788549640c3bf153c756dce073
Ansu-s <130679461+Ansu-s@users.noreply.github.com>
2024-01-11 00:24:03 +0530

    test implementation

--- rbac-service/commons/src/main/java/org/rbac/service/commons/annotations/Authorize.java
+++ rbac-service/commons/src/main/java/org/rbac/service/commons/annotations/Authorize.java
@@ -2,7 +2,13 @@ package org.rbac.service.commons.annotations;
 
 import org.rbac.service.commons.models.Permission;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Authorize {
     String resource() default "";
     Permission permission() default Permission.ALL;
+
+
 }
--- rbac-service/commons/src/main/java/org/rbac/service/commons/resources/ApplicationResource.java
+++ rbac-service/commons/src/main/java/org/rbac/service/commons/resources/ApplicationResource.java
@@ -3,9 +3,25 @@ package org.rbac.service.commons.resources;
 import org.rbac.service.commons.annotations.Authorize;
 import org.rbac.service.commons.models.Permission;
 
+@Authorize(resource = "TEST", permission= Permission.ALL)
 public class ApplicationResource {
     @Authorize(resource = "TEST", permission= Permission.ALL)
     public String getResult(){
         return null;
     }
+
+
+    //              EXTRA CODE FOR DEMONSTRATION (can be removed after usage end)
+    @Authorize(resource = "TEST Database", permission= Permission.READ)
+    public String getUser(){
+        return "read-only";
+    }
+
+    @Authorize(resource = "TEST Production", permission= Permission.WRITE)
+    public String getUser1(){
+        return "write-only";
+    }
+
+    //                  EXTRA CODE END
+
 }
